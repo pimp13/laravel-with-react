6b4519ac6799cfa241767c85fbd95d304a6ebb11
pimp13 <pimp.puma.13@gmail.com>
2026-09-09 15:53:10 +0330

    remove useless import

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\AuthRequest;
 use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
