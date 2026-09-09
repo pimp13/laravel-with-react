40b8bed64d96a09a15cd28ee0b5036d5b18c1df1
pimp13 <pimp.puma.13@gmail.com>
2026-09-09 14:30:55 +0330

    add GuestMiddleware middleware for checking user is not login user is guest

---
+++ app/Http/Middleware/GuestMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class GuestMiddleware
+{
+    public function handle(
+        Request $request,
+        Closure $next
+    ): Response {
+        try {
+            $token = $request->cookie('_token');
+
+            if (!$token) {
+                return $next($request);
+            }
+
+            JWTAuth::setToken($token);
+
+            if (JWTAuth::check()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You are already authenticated.',
+                ], 403);
+            }
+        } catch (\Throwable $e) {
+            return $next($request);
+        }
+
+        return $next($request);
+    }
+}
