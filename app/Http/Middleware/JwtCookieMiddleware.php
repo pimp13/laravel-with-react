38aceda9750e0f13fc1eec4a4c18e3df5c8ecbc0
pimp13 <pimp.puma.13@gmail.com>
2026-09-09 12:39:21 +0330

    add jwt cookie middleware loginc checking exists jwt token and checking exists user

--- app/Http/Middleware/JwtCookieMiddleware.php
+++ app/Http/Middleware/JwtCookieMiddleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtCookieMiddleware
 {
@@ -15,6 +17,31 @@ class JwtCookieMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $token = $request->cookie(config('auth.cookie_name'));
+        if (!$token) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthenticated.'
+            ], 401);
+        }
+
+        try {
+            $user = JWTAuth::setToken($token)->authenticate();
+            if (!$user) {
+                return response()->json([
+                    'message' => 'Unauthenticated.',
+                ], 401);
+            }
+
+            $request->setUserResolver(
+                fn() => $user
+            );
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'Unauthenticated.',
+            ], 401);
+        }
+
         return $next($request);
     }
 }
