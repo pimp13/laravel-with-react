47594c9510502fe3c7f850ea6339c78b1e1ebb77
pimp13 <pimp.puma.13@gmail.com>
2026-09-10 14:01:24 +0330

    Add FA in to lang and publish lang dir

---
+++ lang/fa/validation-attributes.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    */
+    'name' => 'نام',
+    'username' => 'نام کاربری',
+    'email' => 'ایمیل',
+    'first_name' => 'نام',
+    'last_name' => 'نام خانوادگی',
+    'password' => 'گذرواژه',
+    'password_confirmation' => 'تاییدیه گذرواژه',
+    'city' => 'شهر',
+    'state' => 'استان',
+    'country' => 'کشور',
+    'address' => 'آدرس',
+    'phone' => 'تلفن',
+    'mobile' => 'تلفن همراه',
+    'age' => 'سن',
+    'sex' => 'جنسیت',
+    'gender' => 'جنسیت',
+    'day' => 'روز',
+    'month' => 'ماه',
+    'year' => 'سال',
+    'hour' => 'ساعت',
+    'minute' => 'دقیقه',
+    'second' => 'ثانیه',
+    'title' => 'عنوان',
+    'text' => 'متن',
+    'content' => 'محتوا',
+    'description' => 'توضیحات',
+    'date' => 'تاریخ',
+    'time' => 'زمان',
+    'available' => 'موجود',
+    'type' => 'نوع',
+    'img' => 'تصویر',
+    'image' => 'تصویر',
+    'size' => 'اندازه',
+    'color' => 'رنگ',
+    'captcha' => 'کد امنیتی',
+    'price' => 'قیمت',
+    'pic' => 'تصویر',
+    'link' => 'لینک',
+    'mobile_number' => 'شماره تماس',
+    'work_number' => 'شماره تماس ثابت',
+
+];
