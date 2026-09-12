0deaeacd3f988bd135c3585eb09c3fb84bda8d27
pimp13 <pimp.puma.13@gmail.com>
2026-09-12 10:09:53 +0330

    add AuthProvider in AppLayout

--- resources/js/layouts/AppLayout.tsx
+++ resources/js/layouts/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { ToastProvider } from "@/components/ui/Toastalert";
+import { AuthProvider } from "@/contexts/AuthContext";
 import React from "react";
 
 interface AppLayoutProps {
@@ -7,11 +8,13 @@ interface AppLayoutProps {
 
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
-    <ToastProvider>
-      <div>
-        {/* className="mx-auto max-w-7xl px-6 py-8" */}
-        <main>{children}</main>
-      </div>
-    </ToastProvider>
+    <AuthProvider>
+      <ToastProvider>
+        <div>
+          {/* className="mx-auto max-w-7xl px-6 py-8" */}
+          <main>{children}</main>
+        </div>
+      </ToastProvider>
+    </AuthProvider>
   );
 }
