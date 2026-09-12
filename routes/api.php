b4b4b9c83ed680e491a738a233f5ebb34e2ebd1f
pimp13 <pimp.puma.13@gmail.com>
2026-09-12 10:10:44 +0330

    add test api route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Arr;
 
 Route::middleware(['api', 'resolve'])->prefix('v1')->group(function () {
     Route::apiResource('users', UserController::class);
@@ -28,4 +29,12 @@ Route::middleware(['api', 'resolve'])->prefix('v1')->group(function () {
             Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
         });
     });
+});
+
+
+Route::get('/tests', function () {
+    $array = [1, 2, 3, 4, 5];
+    dd(
+        Arr::first($array, fn($val) => $val)
+    );
 });
