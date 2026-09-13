1ea692cfa4650c6593e1bfb42fa45f0e4ab9abb4
pimp13 <pimp.puma.13@gmail.com>
2026-09-13 10:31:12 +0330

    add username random in user model and user table

--- scripts/main.php
+++ scripts/main.php
@@ -2,16 +2,21 @@
 <?php
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class Main
 {
     public static function main()
     {
-        $arr = [1, 2, 3, 4, 5];
-        $res = array_filter($arr, fn($v) => $v % 2 === 0);
+        // $arr = [1, 2, 3, 4, 5];
+        // $res = array_filter($arr, fn($v) => $v % 2 === 0);
 
-        $mystr = 'YHello world';
-        echo str_starts_with($mystr, 'Y') ? 'Yes' : 'No';
+        // $mystr = 'YHello world';
+        // echo str_starts_with($mystr, 'Y') ? 'Yes' : 'No';
+
+        // echo bin2hex(random_bytes(4));
+        // echo str()->random(4);
+        // echo Str::random(4);
     }
 }
 
